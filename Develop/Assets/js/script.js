e8635f411e7f903f4adc5e9ccd9ae14509d0ad97
Tyler Arch <TylerArch@MacBook-Pro.local>
2022-09-25 15:44:27 -0500

    add pseudocode for what still needs to be completed

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -2,6 +2,8 @@
 var today = moment()
 console.log(today.format("MMMM Do YYYY"))
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+
 //Variables
 var hours = [
     "9am",
@@ -14,3 +16,16 @@ var hours = [
     "4pm",
     "5pm",   
 ]
+
+
+//Functions
+
+//save function to local storage
+//text areas for Event
+//functionality for testing whether time has passed
+//add unique ids for time id = "hour-9", "hour-10"
+//for loop to check time and compare to values in hours and color coding them
+//function on page load all saved events are still displayed
+
+//Event listeners
+//save button on click get text from text area and push to local storage and put that in text box for that row and hour
