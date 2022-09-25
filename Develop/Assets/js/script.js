06075369113c31d16af966ec02de7d0a98a28a5a
Tyler Arch <TylerArch@MacBook-Pro.local>
2022-09-25 17:04:22 -0500

    added buttons and input boxes to html along with some formatting

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -16,6 +16,37 @@ var hours = [
     "4pm",
     "5pm",   
 ]
+// Manipulate items in an array
+// Map vs For Each vs For Loop
+const hourArray = [
+    '9am',
+    '10am',
+    '11am',
+    '12pm',
+    '1pm',
+    '2pm',
+    '3pm',
+    '4pm',
+    '5pm'
+];
+
+// Map
+const arrayMadeWithMap = hourArray.map(item => ` ${item}`);
+
+// For Each
+const arrayMadeWithForEach = [];
+hourArray.forEach(item => arrayMadeWithForEach.push(` ${item}`));
+
+// For Loop
+const arrayMadeWithForLoop = [];
+for (i=0; i < hourArray.length; i++) {
+    arrayMadeWithForLoop.push(` ${hourArray[i]}`);
+}
+
+// The results for each are the same!
+console.log(arrayMadeWithMap);
+console.log(arrayMadeWithForEach);
+console.log(arrayMadeWithForLoop);
 
 
 //Functions
