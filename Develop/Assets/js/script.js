18e623ec924b45609e45586c190a2d20a33cc950
Tyler Arch <TylerArch@MacBook-Pro.local>
2022-09-27 19:03:56 -0500

    add functionality to compare time blocks to current time and color code them and update README

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -6,20 +6,20 @@ var saveButtons = document.querySelectorAll("#saveButton");
 
 //Variables
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-var hours = [
-    "9am", moment().startOf('hour').fromNow(),
-    "10am", moment().startOf('hour').fromNow(),
-    "11am", moment().startOf('hour').fromNow(),
-    "12pm", moment().startOf('hour').fromNow(),
-    "1pm", moment().startOf('hour').fromNow(),
-    "2pm", moment().startOf('hour').fromNow(),
-    "3pm", moment().startOf('hour').fromNow(),
-    "4pm", moment().startOf('hour').fromNow(),
-    "5pm", moment().startOf('hour').fromNow(),
-]
-console.log(hours);
+// var hours = [
+//     "9am", moment().startOf('hour').fromNow(),
+//     "10am", moment().startOf('hour').fromNow(),
+//     "11am", moment().startOf('hour').fromNow(),
+//     "12pm", moment().startOf('hour').fromNow(),
+//     "1pm", moment().startOf('hour').fromNow(),
+//     "2pm", moment().startOf('hour').fromNow(),
+//     "3pm", moment().startOf('hour').fromNow(),
+//     "4pm", moment().startOf('hour').fromNow(),
+//     "5pm", moment().startOf('hour').fromNow(),
+// ]
+// console.log(hours);
 
-const hourArray = [
+const hours = [
     '9am',
     '10am',
     '11am',
@@ -54,20 +54,21 @@ function localStorageFunctions() {
 
 //functionality for testing whether time has passed
 //for loop to check time and compare to values in hours and color coding them
-// function timeValid() {
-//     var currentHour = moment().hours(); //Check current timeblock user is in and determines whether it's PAST PRESENT OR FUTURE
-//     $(".time-block").each(function () {
-//         var currentBlock = parseInt($(this).attr("id").split(" ")[0]);
-//         if (currentBlock < currentHour) {
-//             $(this).addClass("past");
-//         } else if (currentBlock === currentHour) {
-//             $(this).removeClass("past");
-//             $(this).addClass("present");
-//         } else {
-//             $(this).removeClass("past");
-//             $(this).removeClass("present");
-//             $(this).addClass("future");
-//         }
-//     });
-// }
+function timeValid() {
+    var currentHour = moment().hours(); //Check current timeblock user is in and determines whether it's PAST PRESENT OR FUTURE
+    $(".time-block").each(function () {
+        var currentBlock = parseInt($(this).attr("id").split(" ")[0]);
+        if (currentBlock < currentHour) {
+            $(this).addClass("past");
+        } else if (currentBlock === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+timeValid();
 
--- README.md
+++ README.md
@@ -10,11 +10,11 @@ Right clicking on the index.html and selecting "Open in default browser" will op
 
 ## Usage
 
-Clicking on an event field allows you to enter the details for an upcoming event. I was unable to complete the functionality of it due to illness but I will be woirking to finish in the next couple days.
+Clicking on an event field allows you to enter the details for an upcoming event. The time block will also light up gray to show if the event is in the past, green to show if the event is in the future, and red to show if the event is in the present hour. The evnts also remain when the page is refreshed.
 
 Here is a screenshot of the landing page:
 
-![Application Preview](https://github.com/TylerArch/project-tracker/blob/main/develop/assets/images/landing-page-screenshot.png?raw=true)
+![Application Preview](https://github.com/TylerArch/project-tracker/blob/main/Develop/Assets/images/landing-page-screenshot.png?raw=true)
 
 Here is the link to the live webpage:
 
